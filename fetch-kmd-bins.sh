2d7b11bffd56746d97d5f863c97b360b16d40ac3
pbca26 <pbca26@gmail.com>
2021-01-28 18:27:09 +0300

    update fetch kmd bins script

--- fetch-kmd-bins.sh
+++ fetch-kmd-bins.sh
@@ -3,14 +3,14 @@ mkdir bin/win64
 mkdir bin/linux64
 mkdir bin/osx
 cd bin/win64
-wget https://github.com/KomodoPlatform/komodo/releases/download/cd_release_0d65a73_research/komodo_0d65a73_research_win.zip
-tar -xvzf komodo_0d65a73_research_win.zip
-rm komodo_0d65a73_research_win.zip
+wget https://github.com/pbca26/komodod/releases/download/cd_release_8688665_dev/komodo_8688665_dev_win.zip
+tar -xvzf komodo_8688665_dev_win.zip
+rm komodo_8688665_dev_win.zip
 cd ../linux64
-wget https://github.com/KomodoPlatform/komodo/releases/download/cd_release_0d65a73_research/komodo_0d65a73_research_linux.zip
-tar -xvzf komodo_0d65a73_research_linux.zip
-rm komodo_0d65a73_research_linux.zip
+wget https://github.com/pbca26/komodod/releases/download/cd_release_8688665_dev/komodo_8688665_dev_linux.zip
+tar -xvzf komodo_8688665_dev_linux.zip
+rm komodo_8688665_dev_linux.zip
 cd ../osx
-wget https://github.com/KomodoPlatform/komodo/releases/download/cd_release_0d65a73_research/komodo_0d65a73_research_osx.zip
-tar -xvzf komodo_0d65a73_research_osx.zip
-rm komodo_0d65a73_research_osx.zip
+wget https://github.com/pbca26/komodod/releases/download/cd_release_8688665_dev/komodo_8688665_dev_osx.zip
+tar -xvzf komodo_8688665_dev_osx.zip
+rm komodo_8688665_dev_osx.zip
