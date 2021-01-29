aa96affddb12d20c2a305a9ecc30b2cfdcb63efe
pbca26 <pbca26@gmail.com>
2021-01-29 23:17:32 +0300

    fix fetch kmd bins script

--- fetch-kmd-bins.sh
+++ fetch-kmd-bins.sh
@@ -4,13 +4,13 @@ mkdir bin/linux64
 mkdir bin/osx
 cd bin/win64
 wget https://github.com/pbca26/komodod/releases/download/cd_release_8688665_dev/komodo_8688665_dev_win.zip
-tar -xvzf komodo_8688665_dev_win.zip
+unzip komodo_8688665_dev_win.zip
 rm komodo_8688665_dev_win.zip
 cd ../linux64
 wget https://github.com/pbca26/komodod/releases/download/cd_release_8688665_dev/komodo_8688665_dev_linux.zip
-tar -xvzf komodo_8688665_dev_linux.zip
+unzip komodo_8688665_dev_linux.zip
 rm komodo_8688665_dev_linux.zip
 cd ../osx
 wget https://github.com/pbca26/komodod/releases/download/cd_release_8688665_dev/komodo_8688665_dev_osx.zip
-tar -xvzf komodo_8688665_dev_osx.zip
+unzip komodo_8688665_dev_osx.zip
 rm komodo_8688665_dev_osx.zip
